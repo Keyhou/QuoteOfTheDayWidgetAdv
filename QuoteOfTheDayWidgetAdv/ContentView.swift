58d7ff0568117fa8d87a315a93462e969e4d3adc
Keyhou <31210145+Keyhou@users.noreply.github.com>
2023-11-22 14:04:42 +0100

    Initial Commit

---
+++ QuoteOfTheDayWidgetAdv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuoteOfTheDayWidgetAdv
+//
+//  Created by Keyhan Mortezaeifar on 22/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuoteOfTheDayWidgetAdv/QuoteOfTheDayWidgetAdvApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteOfTheDayWidgetAdvApp.swift
+//  QuoteOfTheDayWidgetAdv
+//
+//  Created by Keyhan Mortezaeifar on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuoteOfTheDayWidgetAdvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
